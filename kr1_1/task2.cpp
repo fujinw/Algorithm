1e0ea6b844e2f39aab5d0939d49d91a276efea17
Maxim Sharifullin <fujinw@yandex.ru>
2026-02-17 13:11:25 -0800

    kr1_1_t2

--- kr1_1/task2.cpp
+++ kr1_1/task2.cpp
@@ -3,17 +3,12 @@
 using namespace std;
 
 
-bool (int sum, int arr, int size = 5){
-    if (sum% 2 == 1) return false;
-    int target = sum/2;
-
-
-}
-
-
 // Для решения данной задачи предположу, что оценки выставляются, как в школе
-// То есть от 1го до 5ти
-// Тогда задачу можно решить за О(n) при помощи подсчета количества каждой из оценок 
+// То есть от 1го до 5ти( не обязательно брать такой отрезок
+// но потолок оценок/баллов явно должен быть конечным ).
+// Тогда задачу можно решить за О(n) по скорости
+// при помощи подсчета количества каждой из оценок
+// и динамического программирования.
 int main(){
     int n;
     cin >> n;
@@ -24,6 +19,34 @@ int main(){
         cin >> temp;
         supp[temp - 1]++;
     }
-    int sum = supp[0] + supp[1]*2 + supp[2]*3 + supp[3]*4 + supp[4]*5
+    int sum = supp[0] + supp[1]*2 + supp[2]*3 + supp[3]*4 + supp[4]*5;
     
+    if(sum%2 != 0){
+        cout << "NO" << endl;
+        return 0;
+    }
+    int target = sum/2;
+    vector<int> dp(target+1, -1);
+    dp[0] = 0;
+
+    for(int m = 1; m <= 5; m++){ // перебираем в отрезке оценок m(mark)
+        int count = supp[m-1];
+        for(int s = 0; s <= target; s++){
+            if(dp[s] >= 0){
+                dp[s] = count;
+            }else if( s >= m && dp[s - m] > 0){
+                dp[s] = dp[s - m] - 1;
+            }else{
+                dp[s] = -1;
+            }
+        }
+    }
+    
+    if (dp[target] >= 0){
+        cout << "YES" << endl;
+    }else{
+        cout << "NO" << endl;
+    }
+    return 0;
+
 }
